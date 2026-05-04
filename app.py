be9ed56913ffe21a82ceaa3645c74b8e8f2cee9c
sravan-kumarc <sravankumarchagalamarri@gmail.com>
2026-05-04 18:34:18 +0000

    helm altered

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def home():
     return "Hello from CI/CD Pipeline 🚀"
 
+@app.route("/health")
+def health():
+    return "OK", 200
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
